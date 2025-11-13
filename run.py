60f83d774eb29db2efef581129a5b526e42683be
David Cuadrado <95946144+Datians@users.noreply.github.com>
2025-11-13 15:38:50 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 from app import create_app
 from dotenv import load_dotenv
+import os
 
-load_dotenv()  # <- carga variables del .env al entorno
+load_dotenv()
 app = create_app()
 
 if __name__ == "__main__":
-    from os import environ
-    port = int(environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8080))
+    print(f"🚀 Starting Flask on port {port}")
+    app.run(host="0.0.0.0", port=port, debug=False)
+
