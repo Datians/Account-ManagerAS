92e492cbfd09a5719123f8d7ebf3ecc77c343223
David Cuadrado <95946144+Datians@users.noreply.github.com>
2025-11-13 15:42:48 -0500

    Update run.py

--- run.py
+++ run.py
@@ -2,11 +2,15 @@ from app import create_app
 from dotenv import load_dotenv
 import os
 
+# Cargar variables de entorno
 load_dotenv()
+
+# Crear app Flask
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    print(f"🚀 Starting Flask on port {port}")
+    # Tomar el puerto que Railway asigna dinámicamente
+    port = int(os.getenv("PORT", 5000))
+    print(f"🚀 Iniciando Flask en el puerto {port}...")
     app.run(host="0.0.0.0", port=port, debug=False)
 
